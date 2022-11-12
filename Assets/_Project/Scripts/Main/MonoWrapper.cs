368d4a536aafe9e2beb17a553ac120cc70f00c3e
Alexey Burov <burovalexprogrammer@gmail.com>
2022-11-12 09:34:35 +0200

    MonoWrapper  implemented

---
+++ Assets/_Project/Scripts/Main/MonoWrapper.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace _Project.Scripts.Main
+{
+    public abstract class MonoWrapper : MonoBehaviour
+    {
+        private new GameObject gameObject;
+        private new Transform transform;
+        public Transform _transform;
+        public GameObject _gameObject;
+
+        public virtual void Awake()
+        {
+            _transform = base.transform;
+            _gameObject = base.gameObject;
+        }
+    }
+}
