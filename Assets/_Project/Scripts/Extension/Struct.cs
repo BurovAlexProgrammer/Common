6adc53cd1e1ab4bb79ee4d4dbadc1240f90d0cfe
Alexey Burov <burovalexprogrammer@gmail.com>
2022-11-18 05:16:23 +0200

    Implemented toggle

--- Assets/_Project/Scripts/Extension/Struct.cs
+++ Assets/_Project/Scripts/Extension/Struct.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace _Project.Scripts.Extension
@@ -8,9 +7,14 @@ namespace _Project.Scripts.Extension
         public static int RoundInt(this float value) => Mathf.RoundToInt(value);
 
         public static int MultiplyInt(this float value, int multi) => Mathf.RoundToInt(value * multi);
-        
+
         public static int ToMillisecs(this float value) => Mathf.RoundToInt(value * 1000);
+
+        public static void Set(this ref Vector3 vector3, float x = float.NaN, float y = float.NaN, float z = float.NaN)
+        {
+            if (float.IsNaN(x) == false) vector3.x = x;
+            if (float.IsNaN(y) == false) vector3.y = y;
+            if (float.IsNaN(z) == false) vector3.z = z;
+        }
     }
-    
-    
 }
